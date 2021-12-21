12155839607d41d48cf13f824708ea6cce1c3312
Krishna <74672126+krishnasudan909@users.noreply.github.com>
2021-12-21 21:00:38 +0530

    reverting back

--- frontend/src/components/SignUp.js
+++ frontend/src/components/SignUp.js
@@ -35,33 +35,34 @@ export const SignUp = (props) => {
     }
     return (
         <>
-        <h2 className="mt-4 text-center">Create New iNotes Account</h2>
+        <div className='Signup'>
+        <h2 className="text-center">Create New iNotes Account</h2>
         <div className="form-text text-center">We'll never share your credentials with anyone else.</div>
        
-        <form onSubmit={handleSubmit} style={{paddingLeft:"20%", paddingRight:"20%"}} className="container mt-3">
+        <form onSubmit={handleSubmit}  className="container signup-form">
             <div className="row">
             <div className=" col-md-6 mb-2">
-                <label htmlFor="fname" className="form-label"><b>First Name<b style={{color:"red"}}>*</b></b></label>
-                <input type="text" name="fname" className="form-control" onChange={onChange } id="fname" required minLength={2} placeholder="min 2 characters" />
+                <label htmlFor="fname" className="form-label"><b>First Name </b></label>
+                <input type="text" name="fname" className="signup-input form-control" onChange={onChange } id="fname" required minLength={2} placeholder="min 2 characters" />
             </div>
             <div className=" col-md-6 mb-2">
                 <label htmlFor="lname" className="form-label"><b>Last Name</b></label>
-                <input type="text" name="lname" className="form-control" onChange={onChange } id="lname" />
+                <input type="text" name="lname" className="signup-input form-control" onChange={onChange } id="lname" placeholder="optional" />
             </div>
             </div>
             <div className="row">
             <div className="col-md-6 mb-2">
-                <label htmlFor="exampleInputEmail1" className="form-label"><b>Email<b style={{color:"red"}}>*</b></b></label>
-                <input type="email" name="email" className="form-control" onChange={onChange } id="email" aria-describedby="emailHelp" required placeholder="valid email" />
+                <label htmlFor="exampleInputEmail1" className="form-label"><b>Email</b></label>
+                <input type="email" name="email" className="signup-input form-control" onChange={onChange } id="email" aria-describedby="emailHelp" required placeholder="valid email" />
             </div>
             <div className="col-md-6 mb-2">
-                <label htmlFor="phno" className="form-label"><b>Phone Number<b style={{color:"red"}}>*</b></b></label>
-                <input pattern="[6789][0-9]{9}" name="phno" className="form-control" onChange={onChange } id="phno" required minLength={10} maxLength={10} placeholder="min 10 digits" />
+                <label htmlFor="phno" className="form-label"><b>Phone Number </b></label>
+                <input pattern="[6789][0-9]{9}" name="phno" className="signup-input form-control" onChange={onChange } id="phno" required minLength={10} maxLength={10} placeholder="min 10 digits" />
             </div>
             </div>
             <div className="mb-2">
-                <label htmlFor="password" className="form-label"><b>Password<b style={{color:"red"}}>*</b></b></label>
-                <input type="password" name="password" className="form-control" onChange={onChange } id="password" placeholder="min 6 characters" required minLength={6} />
+                <label htmlFor="password" className="form-label"><b>Password </b></label>
+                <input type="password" name="password" className="signup-input form-control" onChange={onChange } id="password" placeholder="min 6 characters" required minLength={6} />
             </div>
             <div className="container text-center mt-2">
             <div className="row">
@@ -71,13 +72,14 @@ export const SignUp = (props) => {
             </div>
             <div className="row">
             <div className="col-md-12">
-            <Link to="/login" className="mx-2" style={{color:"#299930"}}>Already registered? Click here</Link>
+            <Link to="/login" className="mx-2 registered" >Already registered? Click here</Link>
             </div>
             </div>
             </div>
                 
             
         </form>
+        </div>
         </>
     )
 }
